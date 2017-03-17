b79e07f963370675e1625fe4d37fa1fc41940907
Tomas Drencak <tomas@drencak.com>
2017-03-17 06:19:18 +0100

    number, date and keyword coercer

--- env/dev/cljs/rforms/cards.cljs
+++ env/dev/cljs/rforms/cards.cljs
@@ -4,20 +4,71 @@
             [rforms.core :as form]
             [rforms.form :as f]
             [devcards.core :as dc]
-            [rforms.validator :as validator])
+            [rforms.validator :as validator]
+            [rforms.coercer :as coercer])
   (:require-macros
     [reagent.ratom :refer [reaction]]
+    [cljs.test :refer [testing is async]]
     [devcards.core
      :as dc
-     :refer [defcard defcard-doc defcard-rg deftest]]))
-
-(set! goog.LOCALE "sk")
+     :refer [defcard defcard-doc defcard-rg deftest]])
+  (:import
+    [goog.i18n TimeZone]))
 
 (defcard-rg first-card
   [:div>h1 "This is your first devcard!"])
 
 #_(defcard-rg home-page-card
     [core/home-page])
+
+(deftest coercer
+  (testing "Number"
+    (testing "Decimal format"
+      (let [c (coercer/number :decimal "Invalid integer")]
+        (is (= 1 (coercer/from-str c "1")))
+        (is (= "1" (coercer/to-str c 1)))
+        (is (= 1.1 (coercer/from-str c "1,1")))
+        (is (= "1,1" (coercer/to-str c 1.1)))))
+    (testing "Currency format"
+      (let [c (coercer/number :currency "Invalid integer")]
+        (is (= 1 (coercer/from-str c "1 €")))
+        (is (= "1,00 €" (coercer/to-str c 1)))
+        (is (= 1.1 (coercer/from-str c "1,1 €")))
+        (is (= "1,10 €" (coercer/to-str c 1.1)))))
+    (testing "Percent format"
+      (let [c (coercer/number :percent "Invalid percent")]
+        (is (= 0.5 (coercer/from-str c "50 %")))
+        (is (= "50 %" (coercer/to-str c 0.5)))
+
+        (is (= "26 %" (coercer/to-str c 0.256)))
+        (is (= 0.26 (coercer/from-str c "26 %")))))
+    (testing "Decimal percent format #.##%"
+      (let [c (coercer/number "#.##%" "Invalid percent")]
+        (is (= 0.5 (coercer/from-str c "50%")))
+        (is (= "50%" (coercer/to-str c 0.5)))
+
+        (is (= "25,6%" (coercer/to-str c 0.256)))
+        (is (= 0.256 (coercer/from-str c "25,6%"))))))
+(testing "Date"
+  (testing "medium date format"
+    (let [c (coercer/date :medium-date "Invalid date")]
+      (is (= "1. 2. 2010" (coercer/to-str c #inst "2010-02-01T14:34:55.054")))
+      (is (= #inst "2010-02-01" (coercer/from-str c "1. 2. 2010")))))
+  (testing "medium date time format"
+    (let [c (coercer/date :medium-datetime "Invalid date")]
+      (is (= "1. 2. 2010, 15:34:55" (coercer/to-str c #inst "2010-02-01T14:34:55.054")))
+      (is (= #inst "2010-02-01T14:34:55.000" (coercer/from-str c "1. 2. 2010, 15:34:55")))))
+  (testing "medium date time format UTC tz"
+    (let [c (coercer/date :medium-datetime "Invalid date" (TimeZone.createTimeZone 0))]
+      (is (= "1. 2. 2010, 14:34:55" (coercer/to-str c #inst "2010-02-01T14:34:55.054")))
+      (is (= #inst "2010-02-01T14:34:55.000" (coercer/from-str c "1. 2. 2010, 15:34:55")))))
+  )
+  (testing "Keyword"
+    (let [c (coercer/keyword "Invalid keyword")]
+      (is (= :keyword (coercer/from-str c "keyword")))
+      (is (= "keyword" (coercer/to-str c  :keyword)))))
+  #_(testing "Date coercer"
+      (is (= "" (coercer/to-str (coercer/date "dd.MM.yyyy"))))))
 
 (defn input [field label attrs]
   (let [valid?   (form/field-valid? field)
--- src/cljs/rforms/coercer.cljs
+++ src/cljs/rforms/coercer.cljs
@@ -1,11 +1,11 @@
 (ns rforms.coercer
+  (:refer-clojure :exclude [keyword])
   (:require
+    [rforms.validator :as validator]
     [clojure.string :as str]
-    [rforms.validator :as validator])
+    [goog.math])
   (:import
-    [goog.i18n NumberFormat DateTimeFormat DateTimeParse]
-    [goog.math]
-    [clojure.string :as str]))
+    [goog.i18n NumberFormat DateTimeFormat DateTimeParse]))
 
 (defprotocol Coercer
   (to-str [this obj-value])
@@ -27,10 +27,10 @@
         nil)
       nil)))
 
-(deftype NumberCoercer [number-format finite? message]
+(deftype NumberCoercer [number-format finite? error-message]
   Coercer
   (to-str [_ obj-value]
-    (if  (or (not finite?) (goog.math/isFiniteNumber obj-value))
+    (if (or (not finite?) (goog.math/isFiniteNumber obj-value))
       (.format number-format obj-value)
       ""))
   (from-str [this str-value]
@@ -40,10 +40,98 @@
   (validate [this str-value]
     (let [obj-value (from-str this str-value)]
       (if-not (or (not finite?) (goog.math/isFiniteNumber obj-value))
-        [message]
+        [error-message]
         []))))
 
-(defn number-format [number-format message & {:keys [finite]
-                                       :or {finite true}}]
-  (->NumberCoercer number-format finite message))
 
+
+(defn- make-number-format [format]
+  (cond
+    (keyword? format)
+    (-> {:decimal    NumberFormat.Format.DECIMAL
+         :scientific NumberFormat.Format.SCIENTIFIC
+         :percent    NumberFormat.Format.PERCENT
+         :currency   NumberFormat.Format.CURRENCY}
+        (get format)
+        (NumberFormat.))
+
+    :else (NumberFormat. format)))
+
+(defn number [number-format error-message & {:keys [finite]
+                                             :or   {finite true}}]
+  (->NumberCoercer (make-number-format number-format) finite error-message))
+
+
+(deftype DateCoercer [formatter parser error-message tz]
+  Coercer
+  (to-str [_ obj] (if (nil? obj) "" (.format formatter obj tz)))
+  (from-str [_ s] (if (str/blank? s) nil (parse-date parser s)))
+
+  validator/Validator
+  (validate [this str-value]
+    (let [obj-value (from-str this str-value)]
+      (if-not (boolean (or (str/blank? str-value)
+                           (not (nil? (from-str this str-value)))))
+        [error-message]
+        []))))
+
+(defn- make-datetime-format [format]
+  (let [format (if (keyword? format)
+                 (get {
+                       :full-date       DateTimeFormat.Format.FULL_DATE
+                       :long-date       DateTimeFormat.Format.LONG_DATE
+                       :medium-date     DateTimeFormat.Format.MEDIUM_DATE
+                       :short-date      DateTimeFormat.Format.SHORT_DATE
+                       :full-time       DateTimeFormat.Format.FULL_TIME
+                       :long-time       DateTimeFormat.Format.LONG_TIME
+                       :medium-time     DateTimeFormat.Format.MEDIUM_TIME
+                       :short-time      DateTimeFormat.Format.SHORT_TIME
+                       :full-datetime   DateTimeFormat.Format.FULL_DATETIME
+                       :long-datetime   DateTimeFormat.Format.LONG_DATETIME
+                       :medium-datetime DateTimeFormat.Format.MEDIUM_DATETIME
+                       :short-datetime  DateTimeFormat.Format.SHORT_DATETIME}
+                      format))]
+    (DateTimeFormat. format)))
+
+(defn- make-datetime-parse [format]
+  (let [format (if (keyword? format)
+                 (get {
+                       :full-date       DateTimeFormat.Format.FULL_DATE
+                       :long-date       DateTimeFormat.Format.LONG_DATE
+                       :medium-date     DateTimeFormat.Format.MEDIUM_DATE
+                       :short-date      DateTimeFormat.Format.SHORT_DATE
+                       :full-time       DateTimeFormat.Format.FULL_TIME
+                       :long-time       DateTimeFormat.Format.LONG_TIME
+                       :medium-time     DateTimeFormat.Format.MEDIUM_TIME
+                       :short-time      DateTimeFormat.Format.SHORT_TIME
+                       :full-datetime   DateTimeFormat.Format.FULL_DATETIME
+                       :long-datetime   DateTimeFormat.Format.LONG_DATETIME
+                       :medium-datetime DateTimeFormat.Format.MEDIUM_DATETIME
+                       :short-datetime  DateTimeFormat.Format.SHORT_DATETIME}
+                      format))]
+    (DateTimeParse. format)))
+
+(defn date
+  ([format error-message]
+    (date format error-message nil))
+  ([format error-message tz]
+   (->DateCoercer
+     (make-datetime-format format)
+     (make-datetime-parse format)
+     error-message
+     tz)))
+
+(deftype KeywordCoercer [error-message]
+  Coercer
+  (to-str [_ obj] (if (nil? obj) "" (name obj)))
+  (from-str [_ s] (if (str/blank? s) nil (cljs.core/keyword s)))
+
+  validator/Validator
+  (validate [this str-value]
+    (let [obj-value (from-str this str-value)]
+      (if-not (str/blank? str-value)
+        [error-message]
+        []))))
+
+(defn keyword [error-message]
+  (->KeywordCoercer error-message))
